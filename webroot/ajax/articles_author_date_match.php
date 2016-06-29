82895e30622f5f4911f4065060062df6d616f6de
Kostya Kovalchuk <Kostya950@gmail.com>
2016-06-29 14:39:43 +0300

    no message

--- webroot/ajax/articles_author_date_match.php
+++ webroot/ajax/articles_author_date_match.php
@@ -31,12 +31,12 @@ if(isset($_POST['publisher']) OR isset($_POST['date'])){
                         echo "<br><b>";
                         echo $doc['publisher_1']; if(isset($doc['publisher_2']) AND $doc['publisher_2']!=''){echo ', '.$doc['publisher_2'];}
                         $numb = number_format($doc['id'], 0, ',', ' ');
-                        echo "<form method='get' action='/manage/admin/edit_articles/{$doc['id']}'><span class='text-muted'> змінити статтю № </span><input type='submit' class='btn btn-default btn-sm' value='{$doc['id']}'>";
+                        echo "<form method='get' action='/manage/admin/edit_articles/{$doc['id']}'><span class='text-muted'> змінити статтю № </span><input type='submit' class='btn btn-default btn-sm' value='{$doc['id']}'></form>";
                         echo"<br>";
                         echo"<br>";
                     } ?></div>
             </div>
-        </form>
+
         <?php
 
     }
